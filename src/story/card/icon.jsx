305302cb884057a04f77585bf027e0822e6c5b2e
Mauro Moreno <moreno.mauro.emanuel@gmail.com>
2022-06-30 12:21:01 -0300

    v1.0.11

--- src/story/card/icon.jsx
+++ src/story/card/icon.jsx
@@ -1,5 +1,53 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import CImage from '../CImage';
 
-function Icon() {}
+const getIconType = (type) => {
+  const icon = (typeIcon) => {
+    return (
+      <span className="indicator is-text-overlay has-text-white is-size-6">
+        <i className={`icons ${typeIcon}`} />
+      </span>
+    );
+  };
+
+  const types = {
+    video: icon(type),
+    gallery: icon(type),
+    podcast: icon(type),
+    opinion: (
+      <CImage
+        src="/images/quotes.png"
+        alt="Opinión"
+        width="49"
+        height="38"
+        ampLayout="fixed"
+      />
+    ),
+    default: <Fragment />
+  };
+
+  return types[type] || types.default;
+};
+
+function Icon({ story, showIcon }) {
+  return (
+    <Fragment>
+      {showIcon && (
+        <div className="article-icon">
+          {showIcon && getIconType(story.subtype || story.type)}
+        </div>
+      )}
+    </Fragment>
+  );
+}
+
+Icon.propTypes = {
+  story: PropTypes.object,
+  contextPath: PropTypes.string,
+  deployment: PropTypes.func,
+  showIcon: PropTypes.boolean,
+  isAmp: PropTypes.boolean
+};
 
 export default Icon;
